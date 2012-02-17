8e748d7d85fab298fad5a38f67d1cae0adc25b2b
David Hacker <davidhackerdvm@gmail.com>
2012-02-16 22:06:27 -0600

    update dhacker29 mod edition

--- device_targa.mk
+++ device_targa.mk
@@ -129,8 +129,8 @@ PRODUCT_COPY_FILES += \
     device/motorola/targa/prebuilt/app/wifi_tether_v3_1-beta11.apk:system/app/wifi_tether_v3_1-beta11.apk \
     device/motorola/targa/prebuilt/lib/libwtnativetask.so:system/lib/libwtnativetask.so \
     device/motorola/targa/prebuilt/bin/bootsound:system/bin/bootsound \
-#    device/motorola/targa/prebuilt/media/android_audio.mp3:system/media/android_audio.mp3 \
-#    device/motorola/targa/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip \
+    device/motorola/targa/prebuilt/media/android_audio.mp3:system/media/android_audio.mp3 \
+    device/motorola/targa/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip \
 
 # Rootfs files
 PRODUCT_COPY_FILES += \
